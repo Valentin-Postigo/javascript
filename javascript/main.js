a1dd32ce23ecaea885f20f15e9f2579c53f091f8
Valentin Postigo <vpostigo24@gmail.com>
2022-07-19 23:29:38 -0300

    cambios

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+let calculo = prompt("¿Que tipo de calculo quieres hacer?")
+
+if (calculo=="suma") {
+
+        
+
+        while (comeinzo != "EXIT") {
+                comienzo = prompt("Ingrese en las proximas casillas numeros para realizar la suma - Si quiere salir escriba en esta EXIT")
+
+                let primerNumero = parseInt(prompt("Ingrese el primer numero"))
+
+
+                function sumador(num1, num2, num3, num4) {
+                        
+                        let resultado = num1 + num2 + num3 + num4
+
+                        console.log(resultado)
+
+                        return resultado
+
+                        
+                }
+        }
+}else{
+        alert("caca")
+}
