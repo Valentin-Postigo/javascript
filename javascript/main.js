98fde0f50f160d8658c2a06659a729dd43427fe7
Valentin Postigo <vpostigo24@gmail.com>
2022-07-28 22:40:24 -0300

    cambios

--- javascript/main.js
+++ javascript/main.js
@@ -1,81 +1,80 @@
 let calculo = prompt("¿Que tipo de calculo quieres hacer?")
 
-if (calculo=="suma") {
-
 let comienzo
 
 let primerNumero
 
 let segundoNumero
 
+let resultado
+
 let suma
 
+let calculos = ['suma', 'resta', 'division', 'multiplicacion']
+
+for(let i = 0; i < 4; i++){
+        console.log('calculo disponible: '+calculos[i])
+}
+
+if (calculo=="suma") {
+
+
+
         while (comienzo != "EXIT") {
+
                 
-                 comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
+                        comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
 
-                primerNumero = parseInt(prompt("Ingrese un numero"))
+                        primerNumero = parseInt(prompt("Ingrese un numero"))
+                
+                        segundoNumero = parseInt(prompt("Ingrese otro numero"))
+                
+                        suma = primerNumero + segundoNumero
 
-                segundoNumero = parseInt(prompt("Ingrese otro numero"))
+                        resultado = alert(`Su resultado es ${suma}`)
 
-                suma = primerNumero + segundoNumero
-
-                resultado = alert(`Su resultado es ${suma}`)
                                 
         }
 }else if(calculo=="resta"){
 
-let comienzo
-
-let primerNumero
-
-let segundoNumero
-
         while(comienzo != "EXIT"){
-                comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
-                primerNumero = parseInt(prompt("Ingrese un numero"))
 
-                segundoNumero = parseInt(prompt("Ingrese otro numero"))
+                        comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
+                        primerNumero = parseInt(prompt("Ingrese un numero"))
 
-                resta = primerNumero - segundoNumero
+                        segundoNumero = parseInt(prompt("Ingrese otro numero"))
 
-                resultado = alert(`Su resultado es ${resta}`)
+                        resta = primerNumero - segundoNumero
+
+                        resultado = alert(`Su resultado es ${resta}`)
         }
 }else if(calculo=="multiplicacion"){
-let comienzo
-
-let primerNumero
-
-let segundoNumero
 
         while(comienzo != "EXIT"){
-                comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
-                primerNumero = parseInt(prompt("Ingrese un numero"))
 
-                segundoNumero = parseInt(prompt("Ingrese otro numero"))
+                        comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
+                        primerNumero = parseInt(prompt("Ingrese un numero"))
 
-                multiplicacion = primerNumero * segundoNumero
+                        segundoNumero = parseInt(prompt("Ingrese otro numero"))
 
-                resultado = alert(`Su resultado es ${multiplicacion}`)
+                        multiplicacion = primerNumero * segundoNumero
+
+                        resultado = alert(`Su resultado es ${multiplicacion}`)
         }
 }else if(calculo=="division"){
-let comienzo
-
-let primerNumero
-
-let segundoNumero
 
         while(comienzo != "EXIT"){
-                comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
-                primerNumero = parseInt(prompt("Ingrese un numero"))
 
-                segundoNumero = parseInt(prompt("Ingrese otro numero"))
+                        comienzo = prompt("Escriba un numero en la proxima casilla, si quiere salir escriba EXIT")
+                        primerNumero = parseInt(prompt("Ingrese un numero"))
 
-                division = primerNumero / segundoNumero
+                        segundoNumero = parseInt(prompt("Ingrese otro numero"))
 
-                resultado = alert(`Su resultado es ${division}`)
+                        division = primerNumero / segundoNumero
+
+                        resultado = alert(`Su resultado es ${division}`)
         }
 }else{
-        alert("Porfavor ingrese solo (suma, resta, multiplicacion o division)")
-        calculo = prompt("¿Que tipo de calculo quieres hacer?")
-}
+                        alert("Porfavor ingrese solo (suma, resta, multiplicacion o division)")
+                        calculo = prompt("¿Que tipo de calculo quieres hacer?")
+        }
