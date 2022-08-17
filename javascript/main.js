227ae4a121f02bd5398023ecc38c656864bae959
Valentin Postigo <vpostigo24@gmail.com>
2022-08-16 22:55:30 -0300

    cambios

--- javascript/main.js
+++ javascript/main.js
@@ -1,42 +1,24 @@
-const title = document.getElementById('title')
+const selected = document.querySelectorAll(".selected")
+const optionsContainer = document.querySelectorAll(".options-container")
 
-const divisas = ['DOLAR HOY: $290.81' , 'EURO HOY: $308.00' ,'PESO URUGUAYO: $3.26', 'PESO CHILENO: $14.69' ,'REAL BRASILEÑO: $4.02']
+const optionsList = document.querySelectorAll(".option")
 
-const header = document.querySelector('header')
+selected.forEach(selected => {
+        selected.addEventListener("click", () => {
+                optionsContainer.forEach(optionsContainer => {
+                        optionsContainer.classList.toggle("active")
+                })
+        })
+})
 
-let monedas = document.createElement('p')
+optionsList.forEach( o => {
+        o.addEventListener("click", () => {
+                selected.innerHTML = o.querySelector("label").innerHTML
+                optionsContainer.classList.remove("active")
+        })
+})
 
-header.append(monedas)
+localStorage.setItem("dolar", "$295,00")
+localStorage.setItem("euro", "$315,00")
+localStorage.setItem("real","$26,47")
 
-monedas.innerHTML = divisa[Math.floor(Math.random()*divisas.length)]
-
-let divisa = prompt("Que tipo de cambio de divisa quiere realizar? (peso-dolar ; dolar-peso; peso-euro ; euro-peso ; dolar-euro ; euro-dolar ; )")
-
-let monto
-
-let resultado
-
-if(divisa == "peso-dolar"){
-        monto = prompt("Ingrese su monto en pesos")
-
-        resultado = monto * 291.91
-
-        alert(`Su monto es ${resultado} $`)
-
-}else if (divisa == "dolar-peso") {
-        monto = prompt("Ingrese su monto en dolares")
-
-        resultado = monto * 132.64
-
-        alert(`Su monto es ${resultado} $ARS`)
-}else if (divisa == "peso-euro"){
-        monto = prompt("Ingrese su monto en pesos")
-
-        resultado = monto * 298.00
-
-        alert(`Su monto es ${resultado}`)
-} else if (divisa == "euro-peso"){
-        monto = prompt("Ingrese su monto en euros")
-
-        resultado = monto * 135.88 
-}
