40f5e4b43327b5499fe30f00cc5e8d1904d6b1b7
Valentin Postigo <vpostigo24@gmail.com>
2022-08-23 22:04:40 -0300

    cambios

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,65 @@
-const selected = document.querySelectorAll(".selected")
-const optionsContainer = document.querySelectorAll(".options-container")
+const selected1 = document.getElementById("selected1")
+const optionsContainer1 = document.getElementById("options-container1")
 
 const optionsList = document.querySelectorAll(".option")
 
-selected.forEach(selected => {
-        selected.addEventListener("click", () => {
-                optionsContainer.forEach(optionsContainer => {
-                        optionsContainer.classList.toggle("active")
-                })
-        })
+// for(let i = 0; i<selected.length; i++){
+//         selected[i].addEventListener("click", () => {
+//                 for(let i = 0; i<optionsContainer.length; i++){
+//                         optionsContainer[i].classList.toggle("active")
+//                 }
+//         })
+// }
+
+selected1.addEventListener("click", () => {
+                
+                optionsContainer1.classList.toggle("active")
+        
+                
 })
 
 optionsList.forEach( o => {
         o.addEventListener("click", () => {
-                selected.innerHTML = o.querySelector("label").innerHTML
-                optionsContainer.classList.remove("active")
+                selected1.innerHTML = o.querySelector("label").innerHTML
+                optionsContainer1.classList.remove("active")
         })
 })
 
-localStorage.setItem("dolar", "$295,00")
-localStorage.setItem("euro", "$315,00")
-localStorage.setItem("real","$26,47")
+const selected2 = document.getElementById("selected2")
+const optionsContainer2 = document.getElementById("options-container2")
+const optionsList2 = document.querySelectorAll("#option2")
+
+console.log(optionsContainer2)
+selected2.addEventListener("click", () => {
+
+        optionsContainer2.classList.toggle("active")
+
+})
+
+optionsList2.forEach( o => {
+        o.addEventListener("click", () => {
+                selected2.innerHTML = o.querySelector("label").innerHTML
+                optionsContainer2.classList.remove("active")
+        })
+})
+
+
+const button = document.getElementsByClassName("button")
+
+
+
+button.addEventListener("click", (e) => {
+                
+        swal({
+                title: "Calculo realizado",
+                text: "Su valor es: x",
+                icon: "success",
+              });
+
+        
+})
+
+localStorage.setItem("dolar", 295,00)
+localStorage.setItem("euro", 315,00)
+localStorage.setItem("real", 26,47)
 
